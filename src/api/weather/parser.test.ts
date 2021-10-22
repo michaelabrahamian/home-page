9a83576443e3e846a9e457b3259d9e20bec03ff7
Michael Abrahamian <michael.abrahamian7@gmail.com>
2021-10-23 09:06:23 +1100

    refactor(tests): split tests into appropriate files

---
+++ src/api/weather/parser.test.ts
@@ -0,0 +1,21 @@
+import { parseWeatherResponse } from './parser';
+import { DEFAULT_WEATHER_RESPONSE } from '../../test-utils/mocks/weather';
+
+describe('parseWeatherResponse', () => {
+  it('parses a default response payload correcetly', () => {
+    const expectedWeather = {
+      shortDescription: 'Clear',
+      longDescription: 'clear sky',
+      temperature: {
+        average: 291.96,
+        min: 289.68,
+        max: 294.32,
+        feelsLike: 292.09,
+      },
+    };
+
+    const parsedWeather = parseWeatherResponse(DEFAULT_WEATHER_RESPONSE);
+
+    expect(parsedWeather).toEqual(expectedWeather);
+  });
+});
--- src/api/weather/weather.test.ts
+++ src/api/weather/weather.test.ts
@@ -1,27 +1,6 @@
 import { getWeather } from './weather';
-import { parseWeatherResponse } from './parser';
-import { DEFAULT_WEATHER_RESPONSE } from '../../test-utils/mocks/weather';
 
 describe('weather API', () => {
-  describe('parseWeatherResponse', () => {
-    it('parses a default response payload correcetly', () => {
-      const expectedWeather = {
-        shortDescription: 'Clear',
-        longDescription: 'clear sky',
-        temperature: {
-          average: 291.96,
-          min: 289.68,
-          max: 294.32,
-          feelsLike: 292.09,
-        },
-      };
-
-      const parsedWeather = parseWeatherResponse(DEFAULT_WEATHER_RESPONSE);
-
-      expect(parsedWeather).toEqual(expectedWeather);
-    });
-  });
-
   describe('getWeather', () => {
     it('gets weather data from API and processes it correctly', async () => {
       const actualWeather = await getWeather('sydney');
