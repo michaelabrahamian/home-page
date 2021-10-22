c4ce371aca6f97e783f21cbb6eb5b8022de0b3d6
Michael Abrahamian <michael.abrahamian7@gmail.com>
2021-10-22 22:11:11 +1100

    feat(heading): update app heading

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const Header = () => (
+  <header>
+    <Heading variant="h1">Home page</Heading>
+  </header>
+);
+
+const Heading = styled(Typography)`
+  font-size: 48px;
+`;
